0c8a8acab140069b20ed6ba899c1a02627adf455
andygmd <94505778+andygmd@users.noreply.github.com>
2022-03-16 23:41:42 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -4,18 +4,39 @@ var url='https://services3.arcgis.com/6j1KwZfY2fZrfNMR/arcgis/rest/services/Hong
 $.getJSON(url,function(data){
     detail=data.features;
     count=0;
-    show(count);
+    
     var lat=detail[0].attributes.LATITUDE;
     var lon=detail[0].attributes.LONGITUDE;
     lat=parseFloat(lat);
     lon=parseFloat(lon)
     window.onload = function() {
-        document.getElementById('frame').src = 'https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode=&q='+lat+','+lon+'&z=16&output=embed&t=';
+        document.getElementById("frame").src = "https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode=&q="+lat+","+lon+"&z=16&output=embed&t=";
         
     }
+    document.getElementById("myInput").addEventListener("keyup", myFunction);
+    document.getElementById("myInput").addEventListener("keyup", myFunction);
+    function myFunction() { 
+    var table = document.getElementById("myTable");
+    var char = document.getElementById("myInput").value;
+    table.innerHTML = "";
+    for (let i=0;i<detail.length;i++){
+        char=char.toUpperCase(); 
+        if (detail[i].attributes.ENGLISH_NAME.indexOf(char)>=0){
+            var row = table.insertRow();
+            cell = row.insertCell();
+            let a = document.createElement("a");
+            console.log(detail[i].attributes.ENGLISH_NAME);
+            a.innerHTML = detail[i].attributes.ENGLISH_NAME;
+            //a.href = "/show?id="+(detail[i]._id).toString();
+            cell.append(a);
+    }
+    
+    }}
+    
+    myFunction();
 });
 
-function myFunction() {
+/**function myFunction() {
     var input, filter, ul, li, a, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
@@ -30,8 +51,8 @@ function myFunction() {
             li[i].style.display = "none";
         }
     }
-}
-function show(count){  
+}**/
+/**function show(count){  
     var ul = document.getElementById("myUL");
     var li = ul.getElementsByTagName("li");
     for (var i=count; i<7;i++){
@@ -39,31 +60,5 @@ function show(count){
     }
 
 }
+**/
 
-function init() {
-    map = new OpenLayers.Map("basicMap", {
-        controls: [
-                   new OpenLayers.Control.Navigation(),
-                   new OpenLayers.Control.ArgParser(),
-                   new OpenLayers.Control.Attribution()
-               ]
-           });
-    var mapnik         = new OpenLayers.Layer.OSM();
-    var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
-    var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-    var position       = new OpenLayers.LonLat(7.55785346031189,50.3625329673905).transform( fromProjection, toProjection);
-    var zoom           = 3
-    
-    map.addLayer(mapnik);
-    map.setCenter(position, zoom );
-    
-    var markers = new OpenLayers.Layer.Markers( "Markers" );
-    map.addLayer(markers);
-    
-    var marker = new OpenLayers.Marker(position);
-    
-    marker.events.register("click", map , function(e){ alert("click");
-    });
-    
-    markers.addMarker(marker);
-    }
