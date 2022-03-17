d80a3df8ec37f7e3459612ac3e9700cc03140974
andygmd <94505778+andygmd@users.noreply.github.com>
2022-03-17 10:33:51 +0800

    change

--- app.js
+++ app.js
@@ -1,64 +1,60 @@
 var detail;
 var count;
-var url='https://services3.arcgis.com/6j1KwZfY2fZrfNMR/arcgis/rest/services/Hong_Kong_School_Location_and_Information/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
-$.getJSON(url,function(data){
-    detail=data.features;
-    count=0;
+var numb
+var url = 'https://services3.arcgis.com/6j1KwZfY2fZrfNMR/arcgis/rest/services/Hong_Kong_School_Location_and_Information/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json'
+$.getJSON(url, function (data) {
+    detail = data.features;
+    count = 0;
+    console.log(1)
+    var lat = detail[0].attributes.LATITUDE;
+    var lon = detail[0].attributes.LONGITUDE;
+    lat = parseFloat(lat);
+    lon = parseFloat(lon);
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    myTable();
     
-    var lat=detail[0].attributes.LATITUDE;
-    var lon=detail[0].attributes.LONGITUDE;
-    lat=parseFloat(lat);
-    lon=parseFloat(lon)
-    window.onload = function() {
-        document.getElementById("frame").src = "https://maps.google.com.tw/maps?f=q&hl=zh-TW&geocode=&q="+lat+","+lon+"&z=16&output=embed&t=";
-        
-    }
-    document.getElementById("myInput").addEventListener("keyup", myFunction);
-    document.getElementById("myInput").addEventListener("keyup", myFunction);
-    function myFunction() { 
-    var table = document.getElementById("myTable");
-    var char = document.getElementById("myInput").value;
-    table.innerHTML = "";
-    for (let i=0;i<detail.length;i++){
-        char=char.toUpperCase(); 
-        if (detail[i].attributes.ENGLISH_NAME.indexOf(char)>=0){
-            var row = table.insertRow();
-            cell = row.insertCell();
-            let a = document.createElement("a");
-            console.log(detail[i].attributes.ENGLISH_NAME);
-            a.innerHTML = detail[i].attributes.ENGLISH_NAME;
-            //a.href = "/show?id="+(detail[i]._id).toString();
-            cell.append(a);
-    }
-    
-    }}
-    
-    myFunction();
-});
 
-/**function myFunction() {
-    var input, filter, ul, li, a, i, txtValue;
+
+});
+function myFunction() {
+    var input, filter, table, tr, td, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myUL");
-    li = ul.getElementsByTagName("li");
-    for (i = 0; i < li.length; i++) {
-        a = li[i].getElementsByTagName("a")[0];
-        txtValue = a.textContent || a.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-        } else {
-            li[i].style.display = "none";
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
         }
     }
-}**/
-/**function show(count){  
-    var ul = document.getElementById("myUL");
-    var li = ul.getElementsByTagName("li");
-    for (var i=count; i<7;i++){
-        li[i].textContent=detail[i].attributes.ENGLISH_NAME;
+}
+// Find a <table> element with id="myTable":
+function myTable() {
+    var table = document.getElementById("myTable");
+
+    // Create an empty <tr> element and add it to the 1st position of the table:
+    for (var i = 0; i < detail.length; i++) {
+        var a;
+        a = table.insertRow(0).insertCell(0)
+        a.innerHTML = detail[i].attributes.ENGLISH_NAME;
+        a.setAttribute("onclick", "show(" + i + ");");
     }
+}
+//pass value
+function show(no) {
+    window.location.href = "show.html";
+    //school name
+    var schoolname = detail[no].attributes.ENGLISH_NAME;
+    sessionStorage.setItem("schoolName", schoolname);
+    //school no
+    var schoolno = detail[no].attributes.SCHOOL_NO_;
+    sessionStorage.setItem("schoolNo", schoolno);
 
 }
-**/
-
