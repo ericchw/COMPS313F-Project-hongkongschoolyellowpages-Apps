c2a756262bf30324559c3e73d7cfe9a4affee412
andygmd <94505778+andygmd@users.noreply.github.com>
2022-03-17 18:15:39 +0800

    update

--- app.js
+++ app.js
@@ -18,20 +18,23 @@ $.getJSON(url, function (data) {
 
 });
 function myFunction() {
-    var input, filter, table, tr, td, i, txtValue;
+    var input, filter, found, table, tr, td, i, j;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
     for (i = 0; i < tr.length; i++) {
-        td = tr[i].getElementsByTagName("td")[0];
-        if (td) {
-            txtValue = td.textContent || td.innerText;
-            if (txtValue.toUpperCase().indexOf(filter) > -1) {
-                tr[i].style.display = "";
-            } else {
-                tr[i].style.display = "none";
+        td = tr[i].getElementsByTagName("td");
+        for (j = 0; j < td.length; j++) {
+            if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
+                found = true;
             }
+        }
+        if (found) {
+            tr[i].style.display = "";
+            found = false;
+        } else {
+            tr[i].style.display = "none";
         }
     }
 }
