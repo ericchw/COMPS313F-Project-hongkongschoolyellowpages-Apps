05b1dfe6877adff31421cd034f3668cab3de3cfe
ERICCHW <48132385+ERICCHW@users.noreply.github.com>
2022-04-04 01:55:25 +0800

    loading anime for Tnc=> Home,, Home  refresh

--- Flutter test/hongkongschoolyellowpages/lib/screens/home.dart
+++ Flutter test/hongkongschoolyellowpages/lib/screens/home.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:hongkongschoolyellowpages/api/schoolInfoApi.dart';
@@ -16,15 +17,38 @@ class HomeScreen extends StatefulWidget {
   _HomeScreenState createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with TickerProviderStateMixin {
   @override
   void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..addListener(
+        () {
+          setState(() {});
+        },
+      );
+    controller.repeat(reverse: true);
     super.initState();
     _getLanguage();
     _getSchoolInfo();
   }
 
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
   Future<Null> _refresh() async {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    )..addListener(
+        () {
+          setState(() {});
+        },
+      );
+    controller.repeat(reverse: true);
     _isLoading = true;
     _schoolInfoList!.clear();
     _getLanguage();
@@ -43,7 +67,7 @@ class _HomeScreenState extends State<HomeScreen> {
   bool _isLoading = true;
   bool _isEn = false;
   List? _schoolInfoList = [];
-
+  late AnimationController controller;
   Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
 
   Future<void> _getSchoolInfo() async {
@@ -87,134 +111,188 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.yellow,
-        title: Text(
-          _isLoading
-              ? AppLocalizations.of(context)!.loading
-              : AppLocalizations.of(context)!.hongKongSchoolYellowPages,
-          style:
-              const TextStyle(fontWeight: FontWeight.w500, color: Colors.black),
-        ),
-        actions: <Widget>[
-          Padding(
-            padding: EdgeInsets.only(right: 20.0),
-            child: GestureDetector(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => LanguageSettingScreen())).then(
-                  (value) {
-                    setState(
-                      () {
-                        // refresh state
-                        _refresh();
-                      },
-                    );
-                  },
-                );
-              },
-              child: const Icon(
-                Icons.public,
-                color: Colors.black,
-                size: 26.0,
+    return _isLoading
+        ? Scaffold(
+            backgroundColor: Colors.yellow,
+            appBar: AppBar(
+              centerTitle: true,
+              backgroundColor: Colors.yellow,
+              leading: const CircleAvatar(
+                radius: 15.0,
+                backgroundImage: NetworkImage(
+                    "https://static.newmobilelife.com/wp-content/uploads/2018/07/macau1.jpg"),
+                backgroundColor: Colors.transparent,
+              ),
+              title: Text(
+                _isLoading
+                    ? AppLocalizations.of(context)!.loading
+                    : AppLocalizations.of(context)!.hongKongSchoolYellowPages,
+                style: const TextStyle(
+                    fontWeight: FontWeight.w500, color: Colors.black),
               ),
             ),
-          ),
-        ],
-      ),
-      body: RefreshIndicator(
-        backgroundColor: Colors.yellow,
-        color: Colors.green,
-        onRefresh: _refresh,
-        child: Card(
-          child: ListView.builder(
-            shrinkWrap: true,
-            physics: ScrollPhysics(),
-            itemCount: _schoolInfoList!.length,
-            itemBuilder: (context, index) {
-              return _isEn
-                  ? ListTile(
-                      leading: Text(_schoolInfoList![index]['attributes']
-                              ['OBJECTID']
-                          .toString()),
-                      title: Text(
-                        _schoolInfoList![index]['attributes']['ENGLISH_NAME'],
-                        style: const TextStyle(fontSize: 14),
+            body: Row(
+              mainAxisAlignment:
+                  MainAxisAlignment.center, //Center Row contents horizontally,
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, //Center Row contents vertically,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment
+                        .center, //Center Column contents horizontally,
+                    children: <Widget>[
+                      Text(AppLocalizations.of(context)!.loading,
+                          style: Theme.of(context).textTheme.headline6),
+                      CircularProgressIndicator(
+                        value: controller.value,
+                        semanticsLabel: 'Linear progress indicator',
                       ),
-                      subtitle: Text(
-                        _schoolInfoList![index]['attributes']['中文名稱'],
-                        style: const TextStyle(fontSize: 12),
-                      ),
-                      isThreeLine: false,
-                      dense: true,
-                      onTap: () {
-                        Navigator.pushNamed(
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          )
+        : Scaffold(
+            appBar: AppBar(
+              centerTitle: true,
+              backgroundColor: Colors.yellow,
+              leading: const CircleAvatar(
+                radius: 15.0,
+                backgroundImage: NetworkImage(
+                    "https://static.newmobilelife.com/wp-content/uploads/2018/07/macau1.jpg"),
+                backgroundColor: Colors.transparent,
+              ),
+              title: Text(
+                _isLoading
+                    ? AppLocalizations.of(context)!.loading
+                    : AppLocalizations.of(context)!.hongKongSchoolYellowPages,
+                style: const TextStyle(
+                    fontWeight: FontWeight.w500, color: Colors.black),
+              ),
+              actions: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(right: 20.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
                           context,
-                          "Details",
-                          arguments: {
-                            'index': _schoolInfoList![index],
-                            "_isEn": _isEn
-                          },
-                        );
-                      },
-                    )
-                  : ListTile(
-                      leading: Text(_schoolInfoList![index]['attributes']
-                              ['OBJECTID']
-                          .toString()),
-                      title: Text(
-                        _schoolInfoList![index]['attributes']['中文名稱'],
-                        style: const TextStyle(fontSize: 14),
-                      ),
-                      subtitle: Text(
-                        _schoolInfoList![index]['attributes']['ENGLISH_NAME'],
-                        style: const TextStyle(fontSize: 12),
-                      ),
-                      isThreeLine: false,
-                      dense: true,
-                      onTap: () {
-                        Navigator.pushNamed(
-                          context,
-                          "Details",
-                          arguments: {
-                            'index': _schoolInfoList![index],
-                            "isEn": _isEn,
-                          },
-                        );
-                        print(_isEn);
-                      },
-                    );
-            },
-          ),
-        ),
-      ),
-      // bottomNavigationBar: BottomNavigationBar(
-      //   backgroundColor: Colors.yellow,
-      //   items: const <BottomNavigationBarItem>[
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.search),
-      //       label: 'Search',
-      //       backgroundColor: Colors.red,
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.home),
-      //       label: 'Home',
-      //       backgroundColor: Colors.yellow,
-      //     ),
-      //     BottomNavigationBarItem(
-      //       icon: Icon(Icons.public),
-      //       label: 'Lanugage',
-      //       backgroundColor: Colors.blue,
-      //     ),
-      //   ],
-      //   currentIndex: _selectedIndex,
-      //   selectedItemColor: Colors.amber[800],
-      //   onTap: _onItemTapped,
-      // ),
-    );
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  LanguageSettingScreen())).then(
+                        (value) {
+                          setState(
+                            () {
+                              // refresh state
+                              _refresh();
+                            },
+                          );
+                        },
+                      );
+                    },
+                    child: const Icon(
+                      Icons.public,
+                      color: Colors.black,
+                      size: 26.0,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            body: RefreshIndicator(
+              backgroundColor: Colors.yellow,
+              color: Colors.green,
+              onRefresh: _refresh,
+              child: Card(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  physics: ScrollPhysics(),
+                  itemCount: _schoolInfoList!.length,
+                  itemBuilder: (context, index) {
+                    return _isEn
+                        ? ListTile(
+                            leading: Text(_schoolInfoList![index]['attributes']
+                                    ['OBJECTID']
+                                .toString()),
+                            title: Text(
+                              _schoolInfoList![index]['attributes']
+                                  ['ENGLISH_NAME'],
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                            subtitle: Text(
+                              _schoolInfoList![index]['attributes']['中文名稱'],
+                              style: const TextStyle(fontSize: 12),
+                            ),
+                            isThreeLine: false,
+                            dense: true,
+                            onTap: () {
+                              Navigator.pushNamed(
+                                context,
+                                "Details",
+                                arguments: {
+                                  'index': _schoolInfoList![index],
+                                  "_isEn": _isEn
+                                },
+                              );
+                            },
+                          )
+                        : ListTile(
+                            leading: Text(_schoolInfoList![index]['attributes']
+                                    ['OBJECTID']
+                                .toString()),
+                            title: Text(
+                              _schoolInfoList![index]['attributes']['中文名稱'],
+                              style: const TextStyle(fontSize: 14),
+                            ),
+                            subtitle: Text(
+                              _schoolInfoList![index]['attributes']
+                                  ['ENGLISH_NAME'],
+                              style: const TextStyle(fontSize: 12),
+                            ),
+                            isThreeLine: false,
+                            dense: true,
+                            onTap: () {
+                              Navigator.pushNamed(
+                                context,
+                                "Details",
+                                arguments: {
+                                  'index': _schoolInfoList![index],
+                                  "isEn": _isEn,
+                                },
+                              );
+                              print(_isEn);
+                            },
+                          );
+                  },
+                ),
+              ),
+            ),
+            // bottomNavigationBar: BottomNavigationBar(
+            //   backgroundColor: Colors.yellow,
+            //   items: const <BottomNavigationBarItem>[
+            //     BottomNavigationBarItem(
+            //       icon: Icon(Icons.search),
+            //       label: 'Search',
+            //       backgroundColor: Colors.red,
+            //     ),
+            //     BottomNavigationBarItem(
+            //       icon: Icon(Icons.home),
+            //       label: 'Home',
+            //       backgroundColor: Colors.yellow,
+            //     ),
+            //     BottomNavigationBarItem(
+            //       icon: Icon(Icons.public),
+            //       label: 'Lanugage',
+            //       backgroundColor: Colors.blue,
+            //     ),
+            //   ],
+            //   currentIndex: _selectedIndex,
+            //   selectedItemColor: Colors.amber[800],
+            //   onTap: _onItemTapped,
+            // ),
+          );
   }
 }
